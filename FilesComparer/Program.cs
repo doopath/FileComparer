0b02f1e3276e7b8e1d0f5805dd1c2bc854a95e4e
doopath <doopath@gmail.com>
2021-06-30 03:12:13 +0300

    project structure was marked out

---
+++ FilesComparer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+namespace FilesComparer
+{
+    public static class Program
+    {
+        private static Logger _consoleLogger = LogManager.GetLogger("FilesComparer.Program.Console");
+        private static Logger _fileLogger = LogManager.GetLogger("FilesComparer.Program.File");
+
+        public static void Main(string[] args)
+        {
+            _fileLogger.Debug("FilesComparer is starting...");
+
+            LogReceivedArguments(args);
+
+            _fileLogger.Debug("FilesComparer has finished working.\n\n");
+        }
+
+        private static void LogReceivedArguments(string[] args)
+        {
+            _fileLogger.Debug(args.Length < 1 ? "No argumetns were received." : $"Received arguments: {string.Join(" ", args)}.");
+        }
+    }
+}
