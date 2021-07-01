cea08435ae758bbfdb1681a0699899d0be6ab1fb
doopath <doopath@gmail.com>
2021-07-01 16:28:22 +0300

    bug fix #1

--- FileComparer/CompareRequestBuilding.fs
+++ FileComparer/CompareRequestBuilding.fs
@@ -2,7 +2,6 @@
 
 module CompareRequestBuilder =
     open System.IO
-    open Spectre.Console
     open FileComparer.Utils
     open FileComparer.Comparing
     open FileComparer.Colors
@@ -24,13 +23,17 @@ module CompareRequestBuilder =
 
         if (args |> List.contains ap) then
             let pointerIndex = (args |> findArgPointerIndex ap).Value
-            let newArgs = args |> skipItem pointerIndex |> skipItem (pointerIndex + 1)
+
+            // After skipping an argument at index=pointerIndex next one (in the list) will get the same index.
+            // [1; (2); 3; 4] |> skipItem 1 => [1; (3); 4]
+            let newArgs = args |> skipItem pointerIndex |> skipItem pointerIndex
             (newArgs, arg)
         else
             (args, arg)
 
 
     let takePath (def: string) (args: 'a list) =
+        printfn "takePath: %A" args
         let existingDirs = args |> List.where (fun arg -> Directory.Exists (arg.ToString()))
         let path =
             match existingDirs.Length with
@@ -45,18 +48,24 @@ module CompareRequestBuilder =
             (args, path)
 
 
-    let takeColor (def: string) (args: string list) = args |> takeArgument "-c" def
+    let takeColor (def: string) (args: string list) =
+        printfn "takeColor: %A" args
+        args |> takeArgument "-c" def
 
-    let takeWidth (def: string) (args: string list) = args |> takeArgument "-w" def
+    let takeWidth (def: string) (args: string list) =
+        printfn "takeWidth: %A" args
+        args |> takeArgument "-w" def
 
 
     let buildCompareRequest (args: string list) =
         let pathArgsPair = args |> takePath "./"
         let colorArgsPair = (fst pathArgsPair) |> takeColor "purple"
-        let widthArgsPair = (fst colorArgsPair) |> takeWidth ("100")
+        let widthArgsPair = (fst colorArgsPair) |> takeWidth "100"
+
+        printfn "After all: %A" (fst widthArgsPair)
 
         let path = snd pathArgsPair
-        let width = (int) (snd widthArgsPair)
+        let width = int (snd widthArgsPair)
         let color = Colors.colorsMap.[snd colorArgsPair]
 
         (fun () -> Comparer.compare path width color)
